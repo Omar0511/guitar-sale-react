94d8f4dc118b3f2df3ebdae00f5ca6a6f6c2e42d
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-10-29 23:31:36 -0600

    Mostrando el CONTENIDO del CARRITO

--- guitarla/src/components/Header.jsx
+++ guitarla/src/components/Header.jsx
@@ -1,9 +1,11 @@
 // import { Fragment } from "react";
 
+import Guitar from "./Guitar";
+
 // Otra forma de importar Fragment
 // import React from "react";
 
-export default function Header() {
+export default function Header({cart}) {
   // const name = 'probando';
 
   return (
@@ -56,32 +58,42 @@ export default function Header() {
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
-                      <td>
-                        <img
-                          className="img-fluid"
-                          src="./public/img/guitarra_02.jpg"
-                          alt="imagen guitarra"
-                        />
-                      </td>
-                      <td>SRV</td>
-                      <td className="fw-bold">$299</td>
-                      <td className="flex align-items-start gap-4">
-                        <button type="button" className="btn btn-dark">
-                          -
-                        </button>
-                        1
-                        <button type="button" className="btn btn-dark">
-                          +
-                        </button>
-                      </td>
-                      <td>
-                        <button className="btn btn-danger" type="button">
-                          X
-                        </button>
-                      </td>
-                    </tr>
-                  </tbody>
+                    {
+                      cart.map(guitar => (
+                        <tr key={guitar.id}>
+                          <td>
+                            <img
+                              className="img-fluid"
+                              src={`/img/${guitar.image}.jpg`}
+                              alt="imagen guitarra"
+                            />
+                          </td>
+
+                          {/* <td>SRV</td> */}
+                          <td>{guitar.name}</td>
+
+                          {/* <td className="fw-bold">$299</td> */}
+                          <td className="fw-bold">{guitar.price}</td>
+
+                          <td className="flex align-items-start gap-4">
+                            <button type="button" className="btn btn-dark">
+                              -
+                            </button>
+                              {/* 1 */}
+                              {guitar.quantity}
+                            <button type="button" className="btn btn-dark">
+                              +
+                            </button>
+                          </td>
+                          <td>
+                            <button className="btn btn-danger" type="button">
+                              X
+                            </button>
+                          </td>
+                        </tr>
+                      ))
+                    }
+                    </tbody>
                 </table>
 
                 <p className="text-end">
