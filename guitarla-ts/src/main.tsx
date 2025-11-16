b5fc13be313eb8294421d827b1702d7fd53b523e
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-11-15 19:22:17 -0600

    Modificando MAIN para leer TYPESCRIPT

--- guitarla-ts/src/main.tsx
+++ guitarla-ts/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// ! = Assestion not null, non null assertion operator <-  Se le conoce asi a Typescript que el valor no sera nulo ni undefined
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
