b43768ec479af1092858f3bcabdfec537cfdd3af
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-11-17 21:43:08 -0600

    UTILITIES TYPES

--- guitarla-ts/src/types/index.ts
+++ guitarla-ts/src/types/index.ts
@@ -32,3 +32,10 @@ export type CartItem = Guitar & {
 //     quantity: number
 // }
 
+// PICK: permite eleguar ciertos elementos de otro TYPE, ejemplo: <TYPE, 'elemento a heredar' | 'mas de 1 elemento'>
+// export type CartItem = Pick<Guitar, 'id' 1 'name'> 
+// Ponemos las llaves si queremos agregar más elementos
+// export type CartItem = Pick<Guitar, 'id' 1 'name'> & {
+//   quantity: number
+// } 
+// OMIT: es lo contrario, los elementos que ingreses los quitará y agregará los demás
