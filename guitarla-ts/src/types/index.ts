12b35b380cdb7635a29d59a76b6e2457662eccda
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-11-17 21:35:19 -0600

    Heredar y extender un TYPE

--- guitarla-ts/src/types/index.ts
+++ guitarla-ts/src/types/index.ts
@@ -6,9 +6,24 @@ export type Guitar = {
     price: number
 }
 
+// export type CartItem = {
+//     id: number
+//     name: string
+//     image: string
+//     description: string
+//     price: number
+//     quantity: number
+// }
+
+// Al agregar Guitar &, estamos heredando las propiedades de Guitar
 export type CartItem = Guitar & {
     quantity: number
 }
+
+// Herencia en un INTERFACE
+// export interface CartItem extends Guitar & {
+//     quantity: number
+// }
 
 // export type CartItem = Pick<Guitar, 'id' | 'name' | 'price' > & {
 //     quantity: number
