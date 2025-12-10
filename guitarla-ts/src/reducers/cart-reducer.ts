ad4de95184238923cdc5b8726fc87ec621cae5f5
Omar <omar.garcia@morsa.com.mx>
2025-12-10 11:13:01 -0600

    Definiendo el STATE inicial

--- guitarla-ts/src/reducers/cart-reducer.ts
+++ guitarla-ts/src/reducers/cart-reducer.ts
@@ -1,8 +1,18 @@
-import { Guitar } from "../types";
+import { CartItem, Guitar } from "../types";
 
 export type CartActions =
-  { type: 'add-to-cart', payload: {item: Guitar} } |
-  { type: 'remove-from-cart', payload: {id: Guitar['id']} } |
-  { type: 'add-to-cart', payload: {id: Guitar['id']} |
-  { type: 'add-to-cart', payload: {id: Guitar['id']} |
-  { type: 'clear-cart' }
+  { type: 'add-to-cart', payload: { item: Guitar } } |
+  { type: 'remove-from-cart', payload: { id: Guitar['id'] } } |
+  { type: 'add-to-cart', payload: { id: Guitar['id'] } |
+  { type: 'add-to-cart', payload: { id: Guitar['id'] } |
+  { type: 'clear-cart' };
+
+export type CartState = {
+  data: Guitar[]
+  cart: CartItem[]
+};
+
+export const initialState: CartState = {
+  data: db,
+  cart: []
+};
