35b2fb8c7af13fdf5a738d5c61b5b64d61f91f58
Omar <omar.garcia@morsa.com.mx>
2025-12-10 11:21:05 -0600

    Escribiendo el resto de las acciones

--- guitarla-ts/src/reducers/cart-reducer.ts
+++ guitarla-ts/src/reducers/cart-reducer.ts
@@ -1,10 +1,11 @@
+import { db } from "../data/db";
 import { CartItem, Guitar } from "../types";
 
 export type CartActions =
   { type: 'add-to-cart', payload: { item: Guitar } } |
   { type: 'remove-from-cart', payload: { id: Guitar['id'] } } |
-  { type: 'add-to-cart', payload: { id: Guitar['id'] } |
-  { type: 'add-to-cart', payload: { id: Guitar['id'] } |
+  { type: 'decrease-quantity', payload: { id: Guitar['id'] } } |
+  { type: 'increase-quantity', payload: { id: Guitar['id'] } } |
   { type: 'clear-cart' };
 
 export type CartState = {
@@ -15,4 +16,49 @@ export type CartState = {
 export const initialState: CartState = {
   data: db,
   cart: []
+};
+
+export const cartReducer = (
+  state: CartState = initialState,
+  action: CartActions
+) => {
+  if (action.type === "add-to-cart") {
+
+
+    return {
+      ...state
+    };
+  }
+
+  if (action.type === "remove-from-cart") {
+
+
+    return {
+      ...state
+    };
+  }
+
+  if (action.type === "decrease-quantity") {
+
+
+    return {
+      ...state
+    };
+  }
+
+  if (action.type === "increase-quantity") {
+
+
+    return {
+      ...state
+    };
+  }
+
+  if (action.type === "clear-cart") {
+
+
+    return {
+      ...state
+    };
+  }
 };
